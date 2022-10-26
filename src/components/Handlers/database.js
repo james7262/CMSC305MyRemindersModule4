0c196847349025ab4db38ee5cc7e016824c31616
James Bachrach <bachrachj@chc.edu>
2022-10-26 10:53:12 -0400

    October 26th

--- src/components/Handlers/database.js
+++ src/components/Handlers/database.js
@@ -4,6 +4,7 @@ import { openDatabase } from "react-native-sqlite-storage";
 // use hook to create database
 const myRemindersDB = openDatabase({name: 'MyReminders.db'});
 const remindersTableName = 'reminders';
+const prioritiesTableName = 'priorities';
 
 module.exports = {
     // declare function that will create the reminders table
@@ -50,5 +51,47 @@ module.exports = {
             );
         });
     },
+    // Declare function that will create the priorities table.
+    createPrioritiesTable: async function () {
+        // Declare a transaction that will execute a SQL statement.
+        (await myRemindersDB).transaction(txn => {
+            // Execute the SQL.
+            txn.executeSql(
+                `CREATE TABLE IF NOT EXISTS ${prioritiesTableName}(
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    title TEXT,
+                    description TEXT
+                );`,
+                // Arguments needed when using an SQL prepared statement.
+                [],
+                // Callback function to handle results of SQL query.
+                () => {
+                    console.log('Priorities table created successfully');
+                },
+                error => {
+                    console.log('Error creating priorities table ' + error.message);
+                },
+            );
+        });
+    },
 
+    // Declare function that will insert a row into the priorities table
+    addPriority: async function (title, description) {
+        // Declare a transaction that will execute an SQL statement.
+        (await myRemindersDB).transaction(txn => {
+            // Execute the SQL.
+            txn.executeSql(
+                `INSERT INTO ${prioritiesTableName} (title, description) VALUES ("${title}", "${description}")`,
+                // Arguments passed when using SQL prepared statements.
+                [],
+                // Callback function to handle results of SQL query.
+                () => {
+                    console.log(title + " added successfully");
+                },
+                error => {
+                    console.log('Error adding priority ' + error.message);
+                },
+            );
+        });
+    },
 };
--- src/screens/Priorities/index.js
+++ src/screens/Priorities/index.js
@@ -1,15 +1,80 @@
-import React from 'react';
-import { View, TouchableOpacity, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, TouchableOpacity, Text, FlatList } from 'react-native';
+import Priority from '../../components/Priority';
+import { useNavigation } from '@react-navigation/native';
 import styles from './styles';
+// Import openDatabase hook.
+import { openDatabase } from "react-native-sqlite-storage";
+
+// Use hook to create database.
+const myRemindersDB = openDatabase({name: 'MyReminders.db'});
+const prioritiesTableName = 'priorities';
 
 const PrioritiesScreen = props => {
 
+  const navigation = useNavigation();
+  const [priorities, setPriorities] = useState([]);
+
+  useEffect(() => {
+    const listener = navigation.addListener('focus', () => {
+      // Declare an empty array that will store the results of the
+      // SELECT.
+      let results = [];
+      // Declare a transation that will execute the SELECT.
+      myRemindersDB.transaction(txn => {
+        // Execute SELECT.
+        txn.executeSql(
+          `SELECT * FROM ${prioritiesTableName}`,
+          [],
+          // Callback function to handle the results from the
+          // SELECT.
+          (_, res) => {
+            // Get number of rows of data selected.
+            let len = res.rows.length;
+            console.log('Length of priorities ' + len);
+            // If more than one row was returned.
+            if (len > 0){
+              // Loop through the rows.
+              for (let i = 0; i < len; i++){
+                // Push a row of data at a time onto the
+                // results array.
+                let item = res.rows.item(i);
+                results.push({
+                  id: item.id,
+                  title: item.title,
+                  description: item.description,
+                });
+              }
+              // Assign results array to lists state variable.
+              setPriorities(results);
+            } else {
+              // If no rows of data were returned,
+              // set lists state variable to an empty array.
+              setPriorities([]);
+            }
+          },
+          error => {
+            console.log('Error getting priorities ' + error.message);
+          },
+        )
+      });
+    });
+    return listener;
+  });
+
   return (
     <View style={styles.container}>
+      <View>
+        <FlatList 
+          data={priorities}
+          renderItem={({item}) => <Priority post={item} />}
+          keyExtractor={item => item.id}
+        />
+      </View>
         <View style={styles.bottom}>
             <TouchableOpacity 
                 style={styles.button}
-                onPress={() => console.log('Add Priority')}
+                onPress={() => navigation.navigate('Add Priority')}
                 >
                 <Text style={styles.buttonText}>Add Priority</Text>
             </TouchableOpacity>
